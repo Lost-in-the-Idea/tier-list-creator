e35417ee802b558b465f657ae485726fd92d8d9d
Rizq0 <joe_ethan2@hotmail.com>
2026-05-15 23:38:19 +0100

    add: new schema created

---
+++ backend/database/models/user-models.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	DiscordID string `json:"discord_id" gorm:"uniqueIndex"`
+	Username string `json:"username"`
+	Avatar string `json:"avatar"`
+	LastLogin time.Time `json:"last_login"`
+	Sessions []Session `json:"sessions" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"` // association to the Session model
+}
+
+type Session struct {
+	ID uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Token string `json:"token" gorm:"uniqueIndex"`
+	UserID uuid.UUID `json:"user_id" gorm:"type:uuid;index"`
+	LastUsed time.Time `json:"last_used"`
+	ExpiresAt time.Time `json:"expires_at"`
+	}
