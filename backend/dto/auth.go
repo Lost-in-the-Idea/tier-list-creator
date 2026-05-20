72a0c92ab238e1d13c91fc1d8a758b9436f8d675
Rizq0 <joe_ethan2@hotmail.com>
2026-05-20 22:39:05 +0100

    refactor: add DTOs for user and tierlist responses to enhance API structure

---
+++ backend/dto/auth.go
@@ -0,0 +1,12 @@
+package dto
+
+type UserReponse struct {
+	ID        string `json:"id"`
+	DiscordID string `json:"discord_id"`
+	Username  string `json:"username"`
+	Avatar    string `json:"avatar"`
+}
+
+type MessageResponse struct {
+	Message string `json:"message"` // generic message response for success or error messages
+}
