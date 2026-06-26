2ce4fb51729a635c52e4f5eb0ad2371d6af08148
Rizq0 <joe_ethan2@hotmail.com>
2026-06-27 00:42:15 +0100

    refactor: add AuthService, TierlistService, and UserService implementations

---
+++ backend/services/user_service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"gorm.io/gorm"
+
+	"tierlist/database/models"
+)
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(db *gorm.DB) *UserService {
+	return &UserService{db: db}
+}
+
+func (s *UserService) GetAll() ([]models.User, error) {
+	var users []models.User
+	if err := s.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
